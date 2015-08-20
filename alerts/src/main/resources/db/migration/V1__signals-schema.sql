98c05544494daf335580b097d41f82230a469eb3
msldiarra <Kad1diaS>
2015-08-20 04:43:00 +0100

    Added plenty of new tables. Application still works since we can still add new measures but Alert view doesn't seem to work anymore in select mode

--- alerts/src/main/resources/db/migration/V1__signals-schema.sql
+++ alerts/src/main/resources/db/migration/V1__signals-schema.sql
@@ -1,34 +1,171 @@
+--
+-- Table creation
+--
+
 CREATE TABLE Tank (
-  id SERIAL PRIMARY KEY ,
-  name VARCHAR(50) NOT NULL,
-  reference VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NULL,
-  enabled BIT(1) NULL,
-  endDate TIMESTAMP NULL,
-  startDate TIMESTAMP NULL
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL,
+  Reference VARCHAR(15) NOT NULL,
+  StationId INTEGER NOT NULL,
+  Description VARCHAR(255) NOT NULL DEFAULT 'NO_DESCRIPTION',
+  WarningLevelPercentage INTEGER NOT NULL,
+  LiquidTypeId INTEGER NOT NULL,
+  Enabled BIT(1) NOT NULL DEFAULT '1',
+  StartDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE TankEndDate (
+  Id SERIAL PRIMARY KEY,
+  TankId INTEGER NOT NULL,
+  EndDate TIMESTAMP NOT NULL
+);
+
+
 
 CREATE TABLE Measure (
-  id SERIAL PRIMARY KEY,
-  tankId INTEGER NOT NULL,
-  level INTEGER NOT NULL,
-  time TIMESTAMP NULL
+  Id SERIAL PRIMARY KEY,
+  TankId INTEGER NOT NULL,
+  Level INTEGER NOT NULL,
+  Time TIMESTAMP NULL
 );
 
+
+CREATE TABLE LiquidType (
+  Id SERIAL PRIMARY KEY,
+  Reference VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE LiquidDensity (
+  Id SERIAL PRIMARY KEY,
+  Density INTEGER NOT NULL
+);
+
+
+CREATE TABLE Customer (
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE CustomerContact (
+  CustomerId INTEGER NOT NULL,
+  ContactId INTEGER NOT NULL
+);
+
+CREATE TABLE Station (
+  Id SERIAL PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL,
+  Reference VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE CustomerStation (
+  CustomerId INTEGER NOT NULL,
+  StationId INTEGER NOT NULL
+);
+
+CREATE TABLE StationTank (
+  StationId INTEGER NOT NULL,
+  TankId INTEGER NOT NULL
+);
+
+CREATE TABLE Contact (
+  Id SERIAL PRIMARY KEY,
+  FirstName VARCHAR(100) NOT NULL,
+  LastName VARCHAR(100) NOT NULL,
+  ContactInfoId INTEGER NOT NULL,
+  ContactTypeId INTEGER NOT NULL
+);
+
+CREATE TABLE ContactType (
+  Id SERIAL PRIMARY KEY,
+  Type VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE ContactInfo (
+  Id SERIAL PRIMARY KEY,
+  Email VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE TankShape (
+  Id SERIAL PRIMARY KEY,
+  Shape VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE TankSize (
+  Id SERIAL PRIMARY KEY,
+  Width INTEGER NOT NULL,
+  Height INTEGER NOT NULL,
+  ShapeId INTEGER NOT NULL
+);
+
+CREATE TABLE TankTankSize (
+  TankId INTEGER NOT NULL,
+  TankSizeId INTEGER NOT NULL
+);
+
+CREATE TABLE Login (
+    Id SERIAL PRIMARY KEY,
+    Login VARCHAR(50) NOT NULL,
+    Password VARCHAR(50) NOT NULL,
+    Enabled bit(1) NOT NULL DEFAULT '1'
+);
+
+CREATE TABLE ContactLogin (
+    ContactId INTEGER NOT NULL,
+    LoginId INTEGER NOT NULL
+);
 
 CREATE VIEW Alert AS
 SELECT m.id, t.reference AS tankReference, m.level, m.time
 FROM Measure m
 INNER JOIN Tank t ON (t.id = m.id);
 
-CREATE RULE measure_INSERT AS ON INSERT TO Alert DO INSTEAD
+CREATE RULE Meausre_INSERT AS ON INSERT TO Alert DO INSTEAD
   INSERT INTO  Measure (tankId, level, time)
   VALUES ((select Id from Tank WHERE reference = NEW.tankReference), NEW.level, NEW.time)
   RETURNING Measure.id, (SELECT reference FROM Tank WHERE Id = Measure.tankId), Measure.level, Measure.time;
+
 
 --
 -- Constraints
 --
 
+ALTER TABLE Tank
+  ADD CONSTRAINT FK_Tank_LiquidType FOREIGN KEY (LiquidTypeId) REFERENCES LiquidType (Id);
+
 ALTER TABLE Measure
   ADD CONSTRAINT FK_Measure_Tank FOREIGN KEY (tankId) REFERENCES Tank (id);
+
+ALTER TABLE TankEndDate
+  ADD CONSTRAINT FK_TankEndDate_Tank FOREIGN KEY (TankId) REFERENCES Tank (Id);
+
+ALTER TABLE CustomerStation
+  ADD CONSTRAINT FK_CustomerStation_Customer FOREIGN KEY (CustomerId) REFERENCES Customer (Id),
+  ADD CONSTRAINT FK_CustomerStation_Station FOREIGN KEY (StationId) REFERENCES Station (Id);
+
+ALTER TABLE StationTank
+  ADD CONSTRAINT FK_StationTank_Station FOREIGN KEY (StationId) REFERENCES Station (Id),
+  ADD CONSTRAINT FK_StationTank_Tank FOREIGN KEY (TankId) REFERENCES Tank (Id);
+
+ALTER TABLE Contact
+  ADD CONSTRAINT FK_Contact_ContactInfo FOREIGN KEY (ContactInfoId) REFERENCES ContactInfo (Id);
+
+ALTER TABLE Tank
+  ADD CONSTRAINT FK_Tank_Station FOREIGN KEY (StationId) REFERENCES Station (Id);
+
+ALTER TABLE CustomerContact
+  ADD CONSTRAINT FK_CustomerContact_Contact FOREIGN KEY (ContactId) REFERENCES Contact (Id),
+  ADD CONSTRAINT FK_CustomerContact_Customer FOREIGN KEY (CustomerId) REFERENCES Customer (Id);
+
+
+ALTER TABLE TankSize
+  ADD CONSTRAINT FK_TankSize_TankShape FOREIGN KEY (ShapeId) REFERENCES TankShape (Id);
+
+
+ALTER TABLE TankTankSize
+  ADD CONSTRAINT FK_TankTankSize_TankSize FOREIGN KEY (TankId) REFERENCES Tank (Id),
+  ADD CONSTRAINT FK_TankTankSize_Tank FOREIGN KEY (TankSizeId) REFERENCES TankSize (Id);
+
+
+ALTER TABLE ContactLogin
+   ADD CONSTRAINT FK_ContactLogin_Login FOREIGN KEY (LoginId) REFERENCES Login (Id),
+   ADD CONSTRAINT FK_ContactLogin_Contact FOREIGN KEY (ContactId) REFERENCES Contact (Id);
--- alerts/src/main/resources/db/migration/V2__signals-demo-data.sql
+++ alerts/src/main/resources/db/migration/V2__signals-demo-data.sql
@@ -1,5 +1,25 @@
 
-INSERT INTO Tank (name, reference, description, enabled) VALUES
-('Cuve Essence 1', 'A000000001', 'Cuve 001 de Total Sokorodji', '1'),
-('Cuve Gazoil 1', 'A000000002', 'Cuve 002 de Total Sokorodji', '1'),
-('Cuve Sans Plomb 2', 'A000000003', 'Cuve 003 de Total Sokorodji', '1');
+INSERT INTO LiquidType (Reference) VALUES ('OIL');
+INSERT INTO ContactInfo (Email) VALUES
+('mamadou.coulibaly@petrolium.com'),
+('harouna.niang@petrolium.com');
+INSERT INTO ContactType (Type) VALUES
+('Owner'),
+('Manager');
+INSERT INTO Contact (FirstName, LastName, ContactInfoId, ContactTypeId) VALUES
+('Mamadou', 'Coulibaly', 1, 1),
+('Harouna', 'Niang', 2, 1);
+INSERT INTO Customer (Name) VALUES ('Petrolium Limited SA');
+INSERT INTO CustomerContact VALUES (1,1),(1,2);
+INSERT INTO Station (Name, Reference) VALUES ('Banankabougou', 'MLIBKOBNK000001');
+INSERT INTO CustomerStation VALUES (1,1);
+
+
+INSERT INTO TankShape (Shape) VALUES ('cylinder');
+INSERT INTO TankSize (Width, Height, ShapeId) VALUES (15, 35, 1);
+
+
+
+INSERT INTO Tank (Name, Reference, StationId, WarningLevelPercentage, LiquidTypeId, Enabled) VALUES
+('Cuve Essence 1', 'A00000000000001', 1, 30, 1, '1');
+INSERt INTO TankTankSize VALUES (1,1)
